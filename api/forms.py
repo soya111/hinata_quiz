edb628f8e49ba6a7c7cbbafb51dd511f70352f4c
soya111 <soyak20000316@gmail.com>
2020-12-24 23:51:52 +0900

    web側追加編集機能完成

--- api/forms.py
+++ api/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
-from .models import Quiz
+from .models import Quiz, Choice
 
 
 class QuizForm(ModelForm):
     class Meta:
         model = Quiz
         fields = ['title', 'statement', 'thumbnail_image_url']
+
+
+class ChoiceForm(ModelForm):
+    class Meta:
+        model = Choice
+        fields = ['text', 'is_correct']
--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from . import views, main, web
-from .web import QuizListView, QuizDetailView, SignUpView
+from .web import QuizListView, QuizDetailView, SignUpView, QuizCreateView
 
 urlpatterns = [
     path('api/', main.index, name='main_index'),
     path('', QuizListView.as_view(), name='quiz-list'),
     path('quiz/', QuizListView.as_view(), name='quiz-list'),
-    path('quiz/add', QuizListView.as_view(), name='quiz-add'),
+    path('quiz/add/', QuizCreateView.as_view(), name='quiz-add'),
     path('quiz/<int:pk>/', QuizDetailView.as_view(), name='quiz-detail'),
     path('quiz/<int:id>/edit/', web.edit, name='quiz-edit'),
     path('signup/', SignUpView.as_view(), name='signup'),
