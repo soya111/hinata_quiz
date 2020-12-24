506b562697ee03b7a789668e1f94bb20e871c98f
soya111 <soyak20000316@gmail.com>
2020-12-24 13:43:05 +0900

    heroku setting

--- hinata_quiz/settings.py
+++ hinata_quiz/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 import logging
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '.ngrok.io',
+    '.herokuapp.com',
     '127.0.0.1'
 ]
 
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hinata_quiz.urls'
@@ -79,8 +82,12 @@ WSGI_APPLICATION = 'hinata_quiz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'name',
+        'USER': 'user',
+        'PASSWORD': '',
+        'HOST': 'host',
+        'PORT': '',
     }
 }
 
@@ -123,20 +130,31 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if DEBUG:
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(asctime)s %(levelname)s %(message)s',
-    )
-else:
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(asctime)s %(levelname)s %(message)s',
-        filename='/my_log_file.log',
-        filemode='a'
-    )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# if DEBUG:
+#     logging.basicConfig(
+#         level=logging.DEBUG,
+#         format='%(asctime)s %(levelname)s %(message)s',
+#     )
+# else:
+#     logging.basicConfig(
+#         level=logging.DEBUG,
+#         format='%(asctime)s %(levelname)s %(message)s',
+#         filename='/my_log_file.log',
+#         filemode='a'
+#     )
 
 LOGIN_REDIRECT_URL = '/'
+
+
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'].update(db_from_env)
+
+if not DEBUG:
+    SECRET_KEY = os.environ['SECRET_KEY']
+    import django_heroku
+    django_heroku.settings(locals())
 
 
 try:
