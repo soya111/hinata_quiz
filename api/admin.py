0fe5b82a6a181d80f6b03f9a28ce03cb53263154
soya111 <soyak20000316@gmail.com>
2020-12-28 00:46:18 +0900

    admin改善

--- api/admin.py
+++ api/admin.py
@@ -3,7 +3,28 @@ from .models import Quiz, Choice, Nonce, UserInfo
 
 # Register your models here.
 
-admin.site.register(Quiz)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 1
+
+
+class QuizAdmin(admin.ModelAdmin):
+
+    list_display = ('statement', 'choices_str',
+                    'is_approved', 'is_public', 'user')
+    list_editable = ('is_approved',)
+    list_filter = ('is_approved', 'is_public',)
+
+    fieldsets = (
+        (None, {
+            "fields": ['title', 'statement', 'thumbnail_image_url', 'is_approved', 'is_public'],
+        }),
+    )
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Quiz, QuizAdmin)
 admin.site.register(Choice)
 admin.site.register(Nonce)
 admin.site.register(UserInfo)
