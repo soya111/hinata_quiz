388b2cf5369ea9392f7bcf30e41b1328696592fd
soya111 <soyak20000316@gmail.com>
2020-12-30 01:32:13 +0900

    favicon

--- hinata_quiz/settings.py
+++ hinata_quiz/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 )
 
 # if DEBUG:
