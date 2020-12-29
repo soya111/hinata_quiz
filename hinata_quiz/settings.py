65a830cdc967dfd9fc898ecb4b4a295b0dbf6348
soya111 <soyak20000316@gmail.com>
2020-12-30 00:35:50 +0900

    static設定

--- hinata_quiz/settings.py
+++ hinata_quiz/settings.py
@@ -133,6 +133,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles')
+)
+
 # if DEBUG:
 #     logging.basicConfig(
 #         level=logging.DEBUG,
