5956ed551a3886a811a266ecf42cc65c8703f969
soya111 <soyak20000316@gmail.com>
2020-12-30 00:13:05 +0900

    setting database設定修正

--- hinata_quiz/settings.py
+++ hinata_quiz/settings.py
@@ -79,14 +79,18 @@ WSGI_APPLICATION = 'hinata_quiz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'name',
+        'USER': 'user',
+        'PASSWORD': '',
+        'HOST': 'host',
+        'PORT': '',
+    }
+}
 DATABASES['default'] = dj_database_url.config(
     conn_max_age=600, ssl_require=True)
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
